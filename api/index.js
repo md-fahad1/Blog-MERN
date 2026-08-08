8d6eb33edef519bad05061817c7ab132fb587da5
md-fahad1 <20-43328-1@student.aiub.edu>
2026-08-08 09:55:09 +0600

    refactor: clean up index.js by removing commented-out code and improving readability

--- api/index.js
+++ api/index.js
@@ -1,61 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import userRoutes from "./routes/user.route.js";
-// import fbRoutes from "./routes/fb.route.js";
-// import authRoutes from "./routes/auth.route.js";
-// import postRoutes from "./routes/post.route.js";
-// import commentRoutes from "./routes/comment.route.js";
-// import travelRoutes from "./routes/travel.route.js";
-// import uploadRoutes from "./routes/upload.route.js";
-// import cookieParser from "cookie-parser";
-// import path from "path";
-
-// dotenv.config();
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("MongoDb is connected");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// const __dirname = path.resolve();
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000!");
-// });
-
-// app.use("/api/user", userRoutes);
-// app.use("/api/fb", fbRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/post", postRoutes);
-// app.use("/api/comment", commentRoutes);
-// app.use("/api/travel", travelRoutes);
-// app.use("/api/upload", uploadRoutes);
-
-// app.use(express.static(path.join(__dirname, "/client/dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
-
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || "Internal Server Error";
-//   res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
 
 import express from "express";
 import mongoose from "mongoose";
@@ -171,3 +113,62 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRoutes from "./routes/user.route.js";
+// import fbRoutes from "./routes/fb.route.js";
+// import authRoutes from "./routes/auth.route.js";
+// import postRoutes from "./routes/post.route.js";
+// import commentRoutes from "./routes/comment.route.js";
+// import travelRoutes from "./routes/travel.route.js";
+// import uploadRoutes from "./routes/upload.route.js";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDb is connected");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const __dirname = path.resolve();
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000!");
+// });
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/fb", fbRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/post", postRoutes);
+// app.use("/api/comment", commentRoutes);
+// app.use("/api/travel", travelRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
